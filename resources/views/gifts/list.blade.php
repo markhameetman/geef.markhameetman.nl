501f0647505dc581cde828268dd56225e0a2c828
Mark Hameetman <mark@bureau.partners>
2024-04-27 08:08:57 +0200

    wip

--- resources/views/gifts/list.blade.php
+++ resources/views/gifts/list.blade.php
@@ -9,6 +9,7 @@
         <link href="https://cdn.tailwindcss.com/2.2.19/tailwind.min.css" rel="stylesheet">
     </head>
     <body>
+        <h1 class="text-2xl font-bold text-center mt-4">Wensenlijst</h1>
         <ul>
             @foreach($gifts as $gift)
                 <li class="bg-gray-200 p-2 m-2 rounded-lg">
